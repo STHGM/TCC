0c5f3c9d6c4b58a964f66d1644c2d79a5163df81
Rodrigo Moretto <rodrigo-moretto@hotmail.com>
2019-09-22 22:13:58 -0300

    Criação de assets e início metodologias para teste

---
+++ Projeto/TCC-Collisions/Assets/Scripts/colisao.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+public class colisao : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+     void OnCollisionEnter2D(Collision2D col)
+    {
+        Profiler.BeginSample("Minha Colisao");
+        Debug.Log("Colidiu com");
+        print(col.gameObject.name);
+        if (col.gameObject.name == "Floor")
+        {
+            Destroy(gameObject);
+        }
+        Profiler.EndSample();
+    }
+}
