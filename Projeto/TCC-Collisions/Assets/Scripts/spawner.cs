8851a8cbfba4d5a039b6ade5ee3d4c7cb8a43966
Rodrigo Moretto <rodrigo-moretto@hotmail.com>
2019-10-12 14:00:38 -0300

    Removida a velocidade do objeto ao ser instanciado

--- Projeto/TCC-Collisions/Assets/Scripts/spawner.cs
+++ Projeto/TCC-Collisions/Assets/Scripts/spawner.cs
@@ -8,24 +8,21 @@ public class spawner : MonoBehaviour {
 	public float speed = 10f;
 	public float delay = 1.0f;
 	public float repeat = 0.0f;
-    public float velocidade;
 
-    private GameObject clone;
-
-	// Use this for initialization
 	void Start () {
-		// transform.Rotate(rotx, roty, rotz);
 		InvokeRepeating("spawn", repeat, delay);
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		transform.Rotate(Vector3.back, speed * Time.deltaTime);
 		transform.Translate(0.01f, 0, 0);
 	}
 
 	void spawn() {
-        clone = (GameObject)Instantiate(character, transform.position, Quaternion.Euler(0, 0, 0));
-        clone.GetComponent<Rigidbody2D>().velocity = velocidade * transform.localScale.x * clone.transform.right;
+		Instantiate (
+			character,
+			transform.position,
+			Quaternion.identity
+		);
 	}
 }
